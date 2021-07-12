85e3f26d5c4884ea03e69d17dbe0bd353e219791
6210110271 <6210110271@psu.ac.th>
2021-07-12 10:43:06 +0700

    Test6

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Weather from './components/Weather';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Weather from "./components/Weather";
+
+
 export default function App() {
- return (
-  <View style={styles.container}>
-     <Weather zipCode="90110"/>
-     <StatusBar style="auto" />
+
+  return (
+    <View style={styles.container}>
+      <Weather zipCode="90110"/>
+      <StatusBar style="auto"/>
     </View>
- );
+  );
 }
 const styles = StyleSheet.create({
- container: {
- flex: 1,
- backgroundColor: '#fff',
- alignItems: 'center',
- justifyContent: 'center',
- },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
--- components/Weather.js
+++ components/Weather.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React,{useState} from "react";
+import { StyleSheet,View, Text,ImageBackground } from "react-native";
+import Forecast from "./Forcast";
 
+export default function Weather(props) {
+  const [forecastInfo, setForecastInfo] = useState({
+    main: "-",
+    description: "-",
+    temp: 0,
+  })
 
-export default function Weather(props) { 
-    return (
+  return (
+    
+      <ImageBackground source={require("../bg.jpg")} style={styles.backdrop}>
+        <Text>Zip Code</Text>
         <Text>{props.zipCode}</Text>
-    );
-   }
-   
+        <Forecast {...forecastInfo} />
+      </ImageBackground>
+    
+  )
+}
 
-
-  
+const styles = StyleSheet.create({
+  backdrop: {
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  }
+})
